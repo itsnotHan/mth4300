1710054913112d72448493101ccd6bbe2a2761fd
Hanxin Gao <80993827+itsnotHan@users.noreply.github.com>
2024-03-23 01:24:04 -0400

    Shows balance

--- ccAccount.cpp
+++ ccAccount.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
 class CCAccount{
+    private:
+    double balance;
     public:
     std::string cName; //attribute
-    double balance;
+    void changeBalance(double ); //declaration of method
+    double getBalance();
 };
 
+void CCAccount::changeBalance(double x){ //implementation
+    balance=x;
+}
+
+double CCAccount::getBalance(){return balance;}
+
 int main(){
-    CCAccount cat;
-    cat.cName="Mr.Watson";
-    cat.balance=29.95;
+    CCAccount cat; cat.cName="Mr.Watson";
+    cat.changeBalance(29.95);
+    std::cout<<cat.getBalance()<<"\n";
     return 0;
 }
